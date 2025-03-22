82460f51beb87b234846b70be51518ef76aaaa34
abdulrahman <kurdistanhere20@gmail.com>
2025-03-23 01:14:00 +0300

    finished

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const qrtext = document.querySelector("#qrtext");
+const imagebox = document.querySelector(".imagebox");
+const qrimage = document.querySelector("#qrimage");
+
+function generateqr() {
+  if (qrtext.value.length > 0) {
+    qrimage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" +
+      qrtext.value;
+    imagebox.classList.add("showimage");
+    qrtext.classList.remove('error')
+  } else {
+    imagebox.classList.remove("showimage");
+    qrtext.classList.add('error')
+  }
+}
