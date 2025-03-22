c9aff08e95148c6b8abe1710425d2ee8fbfaf186
abdulrahman <kurdistanhere20@gmail.com>
2025-03-23 02:39:44 +0300

    fixing the vercel problem and error, the error was only because of file path!

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const qrimage = document.querySelector("#qrimage");
 function generateqr() {
   if (qrtext.value.length > 0) {
     qrimage.src =
-      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" +
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
       qrtext.value;
     imagebox.classList.add("showimage");
     qrtext.classList.remove('error')
